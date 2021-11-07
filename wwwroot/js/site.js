7b598d4081379a7507a4dd0d97e273ebb1c97eaa
zodiacaca <121059464@qq.com>
2021-11-07 22:33:48 +0800

    m

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const screenLog = document.querySelector('#screen-log')
+let lastTimeStamp = 0
+document.addEventListener('mousemove', function(e) {
+  screenLog.innerText = `
+    Screen X/Y: ${e.screenX}, ${e.screenY}
+    Client X/Y: ${e.clientX}, ${e.clientY}
+    Last interval: ${performance.now() - lastTimeStamp}`
+
+  lastTimeStamp = performance.now()
+})
