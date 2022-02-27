e404d5dd931c0db463c7eed17c35ca1c58337ba5
zodiacaca <121059464@qq.com>
2022-02-28 06:39:43 +0800

    m

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -19,7 +20,17 @@ namespace RazorPagesApp.Pages
 
         public void OnGet()
         {
+            DataSet ds = new DataSet();
 
+            ds.ReadXml("./Pages/counter.xml");
+
+            int hits = Int32.Parse(ds.Tables[0].Rows[0]["hits"].ToString());
+
+            hits += 1;
+
+            ds.Tables[0].Rows[0]["hits"] = hits.ToString();
+
+            ds.WriteXml("./Pages/counter.xml");
         }
     }
 }
